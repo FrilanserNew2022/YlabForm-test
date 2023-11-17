037e57f7dbbabaad9071474f900d5a097bb0a705
Поташов Олег <potashov_2004@mail.ru>
2023-11-17 22:41:21 +0500

    The project has been completed, minor modifications are possible

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,12 @@ function fetchMock(url, options) {
         getUrl() {
             return url;
         },
+        getUser() {
+            return {
+                email: 'Ylab2023@gmail.com',
+                password: 'ivnwFei135'
+            }
+        },
         setUrl(newUrl) {
             url = newUrl;
         },
@@ -31,24 +37,27 @@ function fetchMock(url, options) {
 }
 
 function validateUser(username) {
-
+if (username === '') {
+    alert('fill in the fields')
+    return false
+}
 if (/^\w*[a-zа-яё -]+\w*\d*$/i.test(username) && username.length > 3) {
     return true
 } else {
-    alert('Имя пользователя должно содержать не менее 4 символов и быть написан на латинице')
+    alert('The username must contain at least 4 characters')
     document.getElementById("username").focus();
     return false
 }
 }
 
 function validateEmail(email) {
+    email = String(email)
 if (email === "") {
-    alert("Заполните поле email!");
+    alert("Fill in the email field!");
     return false;
 } else if (!/^([A-Za-z0-9_\-\.]+)@([A-Za-z0-9_\-\.]+\.)+[A-Za-z]{2,6}$/.test(email)) {
 
-    alert("Неверный формат email!");
-    document.getElementById("email").focus();
+    alert("Invalid email format!");
     return false;
 
 }
@@ -57,38 +66,36 @@ return true;
 
 function validatePassword(password) {
 
+    password = String(password)
+
 if (password.length < 8) {
-  alert("Пароль должен быть длиной не менее 8 символов, а так же быть написан на латинице.");
-  document.getElementById("password").focus();
+  alert("The password must be at least 8 characters long, and also be written in Latin.");
   return false;
 }
 if (!password.match(/\d/)) {
-  alert("Ваш пароль должен содержать хотя бы одну цифру, а так же быть написан на латинице.");
-  document.getElementById("password").focus();
+  alert("Your password must contain at least one number and be written in Latin.");
   return false;
 } 
 if (!password.match(/[a-z]/)) {
-  alert("Ваш пароль должен содержать хотя бы одну строчную букву, а так же быть написан на латинице.");
-  document.getElementById("password").focus();
+  alert("Your password must contain at least one lowercase letter and also be written in Latin.");
   return false;
 } 
 if (!password.match(/[A-Z]/)) { 
-  alert("Ваш пароль должен содержать хотя бы одну заглавную букву, а так же быть написан на латинице.");
-  document.getElementById("password").focus();
+  alert("Your password must contain at least one capital letter and be written in Latin.");
   return false;
 } 
 return true;
 }
 
-document.querySelector('.auth__form-submit').addEventListener('click', async (event) => { 
+document.querySelector('.auth__form-submit-create').addEventListener('click', async (event) => { 
 
     event.preventDefault();
 
     const form = event.target.form
 
-    const username = form.querySelector('.auth__form-username').value.trim();
-    const email = form.querySelector('.auth__form-email').value;
-    const password = form.querySelector('.auth__form-password').value;
+    const username = form.querySelector('.auth__form-username-create').value.trim();
+    const email = form.querySelector('.auth__form-email-create').value;
+    const password = form.querySelector('.auth__form-password-create').value;
 
     const formData = new FormData(form);
 
@@ -105,23 +112,67 @@ document.querySelector('.auth__form-submit').addEventListener('click', async (ev
     }
 });
 
+document.querySelector('.auth__form-submit-login').addEventListener('click', async (event) => { 
+
+    event.preventDefault();
+
+    const form = event.target.form
+
+    const emailValue = form.querySelector('.auth__form-email-login').value;
+    const passwordValue = form.querySelector('.auth__form-password-login').value;
+
+    console.log(emailValue)
+
+    if (validateEmail(emailValue) && validatePassword(passwordValue)) {
+        const response = fetchMock('/api/authenticate');
+        if (response.ok) {
+
+            const email = response.getUser().email
+            const password = response.getUser().password
+
+            if(email === emailValue && password === passwordValue) {
+                alert('you have been logged in')
+            } else {
+                alert('password or login do not match, try again')
+            }
+
+        } else {
+            throw new Error('Error authenticating');
+        }    
+    }
+});
+
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
-//Код для работы с визуальной частью
+//Код для работы с визуальной частью a large part of the visual assessment has been added, and validation functions have also been implemented.
 
-let passwordVisibility = false
+let passwordVisibilityCreate = false
+let passwordVisibilityLogin = false
 
-document.querySelector('.auth__form-container').addEventListener('click', (e) => {
-document.querySelector('.auth__form-eye-hide').classList.toggle('hide')
-document.querySelector('.auth__form-eye-open').classList.toggle('hide')
 
-if(!passwordVisibility) {
-    document.querySelector('.auth__form-password').setAttribute('type', 'text')
-} else {
-    document.querySelector('.auth__form-password').setAttribute('type', 'password')
-}
+document.querySelectorAll('.auth__form-container').forEach((el, index) => {
+    el.addEventListener('click', () => {
+        
+        document.querySelectorAll('.auth__form-eye-hide')[index].classList.toggle('hide')
+        document.querySelectorAll('.auth__form-eye-open')[index].classList.toggle('hide')
 
-passwordVisibility = !passwordVisibility
+        if(!passwordVisibilityCreate && index === 0) {
+            document.querySelector('.auth__form-password-create').type = "text"
+            passwordVisibilityCreate = !passwordVisibilityCreate
+        } else if(index === 0) {
+            document.querySelectorAll('.auth__form-password')[index].setAttribute('type', 'password')
+            passwordVisibilityCreate = !passwordVisibilityCreate
+        }
+
+        if(!passwordVisibilityLogin && index === 1) {
+            document.querySelectorAll('.auth__form-password')[index].setAttribute('type', 'text')
+            passwordVisibilityLogin = !passwordVisibilityLogin
+        } else if(index === 1) {
+            document.querySelectorAll('.auth__form-password')[index].setAttribute('type', 'password')
+            passwordVisibilityLogin = !passwordVisibilityLogin
+        }
+
+    })
 })
 
 const buttonCreateAuth = document.querySelector('.auth__header-create')
